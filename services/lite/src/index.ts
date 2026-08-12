541b3b91cf517bb841f13a8386d0e3ed2d101926
yoomarks <lukeluccas6292643@gmail.com>
2026-08-12 15:23:43 +0800

    chore(m7): export conversion analytics runtime

--- services/lite/src/index.ts
+++ services/lite/src/index.ts
@@ -2,4 +2,5 @@ export * from './candidate-qualification.js';
 export * from './content-preparation.js';
 export * from './prepared-action.js';
 export * from './feedback.js';
+export * from './conversion-analytics.js';
 export * from './http.js';
